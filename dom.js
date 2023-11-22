b9a547895758c9a9870089ff814668a34aa45d81
the-point-of-singularity <visionias1208@gmail.com>
2023-11-22 12:51:34 +0530

    applied QuerySelectorAll

--- dom.js
+++ dom.js
@@ -1,10 +1,14 @@
-const headerTitle = document.getElementById('main-header');
-headerTitle.style.borderBottom = 'solid 3px #000';
-const addItem = document.getElementsByClassName('title');
-addItem[0].style.fontWeight = '700';
-addItem[0].style.color = 'green';
-const items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'green';
-for(let i=0;i<items.length;i++) {
-    items[i].style.fontWeight = 'bold';
+//const headerTitle = document.getElementById('main-header');
+//headerTitle.style.borderBottom = 'solid 3px #000';
+//const addItem = document.getElementsByClassName('title');
+//addItem[0].style.fontWeight = '700';
+//addItem[0].style.color = 'green';
+//const items = document.getElementsByClassName('list-group-item');
+//items[2].style.backgroundColor = 'green';
+//for(let i=0;i<items.length;i++) {
+    //items[i].style.fontWeight = 'bold';
+//}
+let odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i=0;i<odd.length;i++) {
+    odd[i].style.backgroundColor = 'blue';
 }
