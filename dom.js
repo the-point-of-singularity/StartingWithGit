6a98774f5605f77123b39dfb8e5bb594e082ba5b
the-point-of-singularity <visionias1208@gmail.com>
2023-11-22 12:02:24 +0530

    3rd commit

---
+++ dom.js
@@ -0,0 +1,5 @@
+const headerTitle = document.getElementById('main-header');
+headerTitle.style.borderBottom = 'solid 3px #000';
+const addItem = document.getElementsByClassName('title');
+addItem[0].style.fontWeight = '700';
+addItem[0].style.color = 'green';
