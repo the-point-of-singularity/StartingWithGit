99eb1499bfda7aa868feecbdf1f15e62ff31f2da
the-point-of-singularity <visionias1208@gmail.com>
2023-11-24 15:13:53 +0530

    traversing the dom

--- dom.js
+++ dom.js
@@ -8,7 +8,34 @@
 //for(let i=0;i<items.length;i++) {
     //items[i].style.fontWeight = 'bold';
 //}
-let odd = document.querySelectorAll('li:nth-child(odd)');
-for(let i=0;i<odd.length;i++) {
-    odd[i].style.backgroundColor = 'blue';
-}
+//let odd = document.querySelectorAll('li:nth-child(odd)');
+//for(let i=0;i<odd.length;i++) {
+//    odd[i].style.backgroundColor = 'blue';
+//}
+var itemList = document.querySelector('#items');
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor='#f4f4f4';
+console.log(itemList.parentNode.parentNode);
+console.log(itemList.parentElement);
+console.log(itemList.childNodes);
+console.log(itemList.children);
+console.log(itemList.firstChild);
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent='Hello 1';
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent="Hello 4";
+console.log(itemList.nextSibling);
+console.log(itemList.nextElementSibling);
+console.log(itemList.previousSibling);
+console.log(itemList.previousElementSibling);
+itemList.previousElementSibling.style.color='green';
+var newDiv = document.createElement('div');
+newDiv.className='hello';
+console.log(newDiv);
+newDiv.id='hello1';
+newDiv.setAttribute('title','Hello Div');
+var newDivText=document.createTextNode('Hello World');
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header.container');
+var h1=document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
