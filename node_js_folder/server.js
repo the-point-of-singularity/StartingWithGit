969f3e3f2ad6de97b195870c5e2f0c76cd644412
the-point-of-singularity <visionias1208@gmail.com>
2024-03-11 19:12:55 +0530

    Routing Requests

--- node_js_folder/server.js
+++ node_js_folder/server.js
@@ -1,18 +1,19 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    if(req.url==='/node') {
-        res.write('<body><h1>Hello from my Node.js server !</h1></body>');
+    const url = req.url;
+    if (url === "/") {
+        res.write("<html>");
+        res.write("<head><title>Enter a Message</title></head>");
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>');
+        res.write("</html");
+        return res.end();
     }
-    if(req.url==='/home') {
-        res.write('<body><h1>Hello from my Home Page !</h1></body>'); 
-    }
-    res.write('</html');
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body><h1>Hello from my Node.js server !</h1></body>");
+    res.write("</html");
     res.end();
 });
 
