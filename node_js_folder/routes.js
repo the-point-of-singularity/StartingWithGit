6b92594c46703b0ab5f966d2f906249a1cb33fd4
the-point-of-singularity <visionias1208@gmail.com>
2024-03-19 18:06:30 +0530

    reading from the file

--- node_js_folder/routes.js
+++ node_js_folder/routes.js
@@ -1,19 +1,27 @@
 const fs = require("fs");
 
-
 const requestHandler = (req, res) => {
-const url = req.url;
+    const url = req.url;
+    console.log(url);
     const method = req.method;
     if (url === "/") {
-        res.write("<html>");
-        res.write("<head><title>Enter a Message</title></head>");
-        res.write(
-            '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>'
-        );
-        res.write("</html");
-        return res.end();
-    }
-    if (url === "/message" && method === "POST") {
+        console.log("Succesfully entered here!");
+        fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            res.write("<html>");
+            res.write("<head><title>Enter a Message</title></head>");
+            res.write(`<body>${data}</body>`);
+            res.write(
+                '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>'
+            );
+            res.write("</html");
+            return res.end();
+            
+        });
+        
+    } else if (url === "/message" && method === "POST") {
         const body = [];
         req.on("data", (chunk) => {
             console.log(chunk);
@@ -28,17 +36,17 @@ const url = req.url;
                 res.setHeader("Location", "/");
                 return res.end();
             });
-            
         });
+    } 
+    else {
+        res.setHeader("Content-Type", "text/html");
+        res.write("<html>");
+        res.write("<head><title>My First Page</title></head>");
+        res.write("<body><h1>Hello from my Node.js server !</h1></body>");
+        res.write("</html");
+        res.end();
     }
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><h1>Hello from my Node.js server !</h1></body>");
-    res.write("</html");
-    res.end();
-
 };
 
-module.exports = requestHandler;
 
+module.exports = requestHandler;
