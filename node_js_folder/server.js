e6a5c04ed2c9939ecafd05f428f98cfec32a323c
the-point-of-singularity <visionias1208@gmail.com>
2024-03-19 13:16:04 +0530

    event driven code execution

--- node_js_folder/server.js
+++ node_js_folder/server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const fs = require('fs');
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
     const url = req.url;
@@ -7,25 +7,27 @@ const server = http.createServer((req, res) => {
     if (url === "/") {
         res.write("<html>");
         res.write("<head><title>Enter a Message</title></head>");
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>');
+        res.write(
+            '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>'
+        );
         res.write("</html");
         return res.end();
     }
-    if(url==='/message' && method==='POST') {
+    if (url === "/message" && method === "POST") {
         const body = [];
-        req.on('data', (chunk) => {
+        req.on("data", (chunk) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on("end", () => {
             const parsedBody = Buffer.concat(body).toString();
             console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            const message = parsedBody.split("=")[1];
+            fs.writeFileSync("message.txt", message);
+            res.statusCode = 302;
+            res.setHeader("Location", "/");
+            return res.end();
         });
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
     }
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
