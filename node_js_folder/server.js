3d9ab3110b8e4b578b44573c35eb2489c1483a64
the-point-of-singularity <visionias1208@gmail.com>
2024-03-20 13:54:18 +0530

    Handling different routes using middlewares

--- node_js_folder/server.js
+++ node_js_folder/server.js
@@ -1,21 +1,19 @@
-//const http = require("http");
-
 const express = require('express');
 const app = express();
-// const routes = require('./routes.js');
 
-app.use((req, res, next) => {
-   console.log("In the middleware!");
-   next();// Allows the request to continue to the next middleware..
+app.use('/', (req, res, next) => {
+    console.log("This always runs!");
+    next();
 });
 
-app.use((req, res, next) => {
-   console.log("In another middleware!");
-   //....send a response...
+app.use('/add-product',(req, res, next) => {
+   console.log("In the add-product middleware!");
+   res.send('<h1>We are inside the add-product page!</h1>');
+});
+
+app.use('/',(req, res, next) => {
+   console.log("In the general middleware!");
    res.send('<h1>Hello from express.js!</h1>');
 });
-
-// const server = http.createServer(app);
-// server.listen(3000);//both these lines can be clubbed together by app.listen(3000); and the http import can be removed
 
 app.listen(3000);
