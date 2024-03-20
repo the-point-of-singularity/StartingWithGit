c41cfe097c73bcbf5191722593867a4b13220b7d
the-point-of-singularity <visionias1208@gmail.com>
2024-03-20 17:00:26 +0530

    Using express router for different routes

--- node_js_folder/server.js
+++ node_js_folder/server.js
@@ -2,19 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes = require('./routes_folder/admin.js');
+const shopRoutes = require('./routes_folder/shop.js');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req, res, next) => {
-   res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req, res, next) => {
-   res.send('<h1>Hello from express.js!</h1>');
-});
+app.use(shopRoutes);
 
 app.listen(3000);
