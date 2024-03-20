66c487108c1c9426db84e911b52587d5a16f0098
the-point-of-singularity <visionias1208@gmail.com>
2024-03-20 14:37:55 +0530

    Using two input fields

--- node_js_folder/server.js
+++ node_js_folder/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/add-product',(req, res, next) => {
-   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+   res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">Add Product</button></form>');
 });
 
 app.post('/product', (req, res, next) => {
