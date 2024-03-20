1eb3eefc1fc8ff08ad6062c347bd63cbcd37f0fc
the-point-of-singularity <visionias1208@gmail.com>
2024-03-20 14:28:35 +0530

    Parsing incoming requests using body-parser

--- node_js_folder/server.js
+++ node_js_folder/server.js
@@ -1,18 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log("This always runs!");
-    next();
-});
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/add-product',(req, res, next) => {
-   console.log("In the add-product middleware!");
-   res.send('<h1>We are inside the add-product page!</h1>');
+   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
 app.use('/',(req, res, next) => {
-   console.log("In the general middleware!");
    res.send('<h1>Hello from express.js!</h1>');
 });
 
