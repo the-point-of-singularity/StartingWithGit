d6a24a95641348618345a7c772117fd063ff66b9
the-point-of-singularity <visionias1208@gmail.com>
2024-03-20 12:25:00 +0530

    Adding express.js

--- node_js_folder/server.js
+++ node_js_folder/server.js
@@ -1,8 +1,9 @@
 const http = require("http");
 
+const express = require('express');
+const app = express();
+// const routes = require('./routes.js');
 
-const routes = require('./routes.js');
-
-const server = http.createServer(routes);
+const server = http.createServer(app);
 
 server.listen(3000);
