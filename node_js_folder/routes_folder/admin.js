9cf7094e17e6238cdc44ad4c9c80e4fdc5318279
the-point-of-singularity <visionias1208@gmail.com>
2024-03-20 17:27:06 +0530

    Filtering admin routes

--- node_js_folder/routes_folder/admin.js
+++ node_js_folder/routes_folder/admin.js
@@ -2,13 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 
+
+//The implicit route is /admin/add-product
 router.get("/add-product", (req, res, next) => {
     res.send(
-        '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">Add Product</button></form>'
+        '<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="number" name="quantity"><button type="submit">Add Product</button></form>'
     );
 });
 
-router.post("/product", (req, res, next) => {
+
+//The implicit route is /admin/add-product
+router.post("/add-product", (req, res, next) => {
     console.log(req.body);
     res.redirect("/");
 });
--- node_js_folder/server.js
+++ node_js_folder/server.js
@@ -7,7 +7,7 @@ const shopRoutes = require('./routes_folder/shop.js');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 
 app.use(shopRoutes);
 
