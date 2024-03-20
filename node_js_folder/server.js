2fe0f2a6f6dbe5c92f5c32c8dd42b0420fa27e5e
the-point-of-singularity <visionias1208@gmail.com>
2024-03-20 12:48:05 +0530

    Express middleware

--- node_js_folder/server.js
+++ node_js_folder/server.js
@@ -4,6 +4,16 @@ const express = require('express');
 const app = express();
 // const routes = require('./routes.js');
 
+app.use((req, res, next) => {
+   console.log("In the middleware!");
+   next();// Allows the request to continue to the next middleware..
+});
+
+app.use((req, res, next) => {
+   console.log("In another middleware!");
+   //....send a response...
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
