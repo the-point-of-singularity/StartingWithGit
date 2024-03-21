536e53680db3ebbbb8dee6755b7c32e69c0cbc75
the-point-of-singularity <visionias1208@gmail.com>
2024-03-21 14:47:51 +0530

    Path file for root directory added separately!

--- node_js_folder/routes/admin.js
+++ node_js_folder/routes/admin.js
@@ -2,16 +2,18 @@ const path = require('path');
 
 const express = require("express");
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 
-//The implicit route is /admin/add-product
+
 router.get("/add-product", (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 
-//The implicit route is /admin/add-product
+
 router.post("/add-product", (req, res, next) => {
     console.log(req.body);
     res.redirect("/");
