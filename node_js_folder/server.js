b02b2ca49aff1b9f8909039a8052134a54d73c99
the-point-of-singularity <visionias1208@gmail.com>
2024-03-21 15:46:08 +0530

    Serving css pages statically

--- node_js_folder/server.js
+++ node_js_folder/server.js
@@ -8,6 +8,7 @@ const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(adminRoutes);
 
