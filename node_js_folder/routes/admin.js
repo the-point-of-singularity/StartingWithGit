62812e52e496a0940c29d267e8cf3140736ed50b
the-point-of-singularity <visionias1208@gmail.com>
2024-03-21 17:51:53 +0530

    Added a Contact-us functionality

--- node_js_folder/routes/admin.js
+++ node_js_folder/routes/admin.js
@@ -12,11 +12,23 @@ router.get("/add-product", (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
+router.get("/contact-us", (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'contact-us.html'));
+});
+
+router.get("/success", (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'success.html'));
+});
+
 
 
 router.post("/add-product", (req, res, next) => {
     console.log(req.body);
     res.redirect("/");
+});
+
+router.post("/contact-us", (req, res, next) => {
+    res.redirect("/success");
 });
 
 module.exports = router;
