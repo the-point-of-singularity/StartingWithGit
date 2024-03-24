c3ff87f3f59a40cc76381d6866032158bc5d72ab
the-point-of-singularity <visionias1208@gmail.com>
2024-03-24 19:44:46 +0530

    Storing and fetching data from files!

--- practice_backend/models/product.js
+++ practice_backend/models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../util/path");
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +9,27 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        const p = path.join(rootDir, "data", "products.json");
+        fs.readFile(p, (err, fileContent) => {
+            //console.log(fileContent);
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        const p = path.join(rootDir, "data", "products.json");
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
     }
 };
