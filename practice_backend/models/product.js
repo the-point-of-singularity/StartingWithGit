554d3091e1b9003e2906a07d80ae124104b9a44c
the-point-of-singularity <visionias1208@gmail.com>
2024-03-24 20:01:06 +0530

    Refactoring the file storage code

--- practice_backend/models/product.js
+++ practice_backend/models/product.js
@@ -3,19 +3,24 @@ const path = require("path");
 
 const rootDir = require("../util/path");
 
+const p = path.join(rootDir, "data", "products.json");
+
+const getProductsfromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        const p = path.join(rootDir, "data", "products.json");
-        fs.readFile(p, (err, fileContent) => {
-            //console.log(fileContent);
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsfromFile((products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
@@ -24,12 +29,6 @@ module.exports = class Product {
     }
 
     static fetchAll(cb) {
-        const p = path.join(rootDir, "data", "products.json");
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductsfromFile(cb);
     }
 };
