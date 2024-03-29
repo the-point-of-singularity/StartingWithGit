44e718e7a22a3309f61f268b9dd0d3f31f253295
the-point-of-singularity <visionias1208@gmail.com>
2024-03-29 17:52:54 +0530

    Learning to use mySQL Database

---
+++ Udemy_project_v2/util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Aluphala@110319951993'
+});
+
+module.exports = pool.promise();
